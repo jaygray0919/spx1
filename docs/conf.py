c566a9e252f80e6955c4fac1539d61fcf44edca8
jay gray <jaygray0919@gmail.com>
2023-04-28 17:40:28 -0500

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,6 @@ extensions = [
     'sphinx_tabs.tabs',
     ]
 
-
-
 intersphinx_mapping = {
          "ldp": ("https://isolveit.github.io/linked-data-patterns/", None),
   "complexity": ("https://isolveit.github.io/chem-complexity-diet/", None),
@@ -255,7 +253,7 @@ html_short_title = 'Sphinx Catalog'
 
 # The name of an image file (within the static path) to use as favicon of the docs.
 # This file should be a Windows icon file (.ico) being 16x16 or 32x32 pixels large.
-#html_favicon = "Ontomatica.png"
+html_favicon = "Ontomatica.png"
 
 # Add any paths that contain custom static files (such as style sheets) here, relative to this directory.
 # They are copied after the built-in static files, so a file named "default.css" will overwrite the built-in "default.css".
